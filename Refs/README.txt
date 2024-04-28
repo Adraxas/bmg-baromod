8f929aecd20ce343d95e731de2bb7aa5daa77cda
Adraxas <dragonjunk45@gmail.com>
2024-04-27 22:21:50 -0700

    Experimenting

---
+++ Refs/README.txt
@@ -0,0 +1,19 @@
+- The libraries for this folder must be downloaded separately.
+- If you have the Refs.zip file, you should extract it here.
+- The file-folder structure should match the following:
+./Linux/Barotrauma.dll
+./Linux/DedicatedServer.dll
+./Windows/Barotrauma.dll
+./Windows/DedicatedServer.dll
+./OSX/Barotrauma.dll
+./OSX/DedicatedServer.dll
+./0Harmony.dll
+./Farseer.NetStandard.dll
+./Lidgren.NetStandard.dll
+./Mono.Cecil.dll
+./MonoGame.Framework.Linux.NetStandard.dll
+./MonoGame.Framework.MacOS.NetStandard.dll
+./MonoGame.Framework.Windows.NetStandard.dll
+./MonoMod.Common.dll
+./MoonSharp.Interpreter.dll
+./XNATypes.dll
--- ServerProject/ServerSource/Plugin.cs
+++ ServerProject/ServerSource/Plugin.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Barotrauma;
+using HarmonyLib;
 
 namespace MyModName
 {
     public partial class Plugin : IAssemblyPlugin
     {
-        // Server-specific code
+        luac
+    }
+    public static class Patcher
+    {
+        public static void DoPatching()
+        {
+            var harmony = new Harmony("com.aptitude.baro");
+            harmony.PatchAll();
+
+        }
+    }
+    [HarmonyPatch(typeof(Barotrauma.DebugConsole.Command))]
+    [HarmonyPatch(nameof(Barotrauma.DebugConsole.Command))]
+    static class Patch
+    {
+        [HarmonyPostfix]
+        static void NewCommands()
+        {
+            var meth = Traverse.Create<Command>().Method("InitProjectSpecific").GetValue();
+            meth.commands.add
+        }
     }
 }
